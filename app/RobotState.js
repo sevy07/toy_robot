641d9956730f816630d3db4513ff74b764c90f15
sevy07 <sevy07@msn.com>
2018-11-17 15:15:06 +0000

    FEATURE-1 implementing RobotState

--- app/RobotState.js
+++ app/RobotState.js
@@ -5,9 +5,38 @@
  * 
  * @author Jean-Yves Chanal
  */
+const path = require("path");
+const config = require(path.join("..", "config", "config.json"));
+
+const maxX = config.table.width;
+const maxY = config.table.height;
 
 class RobotState {
 
+  /**
+   * @constructor RobotState
+   * @param {Number} x 
+   * @param {Number} y 
+   * @param {Number} f 
+   */
+  constructor(x, y, f) {
+
+    this.f = f;
+    this.isOnTheTable = true;
+
+    if (x < 0 || x > maxX) {
+      this.isOnTheTable = false;
+      this.x = 0;
+    } else {
+      this.x = x;
+    }
+    if (y < 0 || y > maxY) {
+      this.isOnTheTable = false;
+      this.y = 0;
+    } else {
+      this.y = y;
+    }
+  }
 }
 
 module.exports = RobotState;
--- test/RobotState.spec.js
+++ test/RobotState.spec.js
@@ -2,27 +2,50 @@ const path = require("path");
 const expect = require("chai").expect;
 
 const RobotState = require(path.join("..", "app", "RobotState"));
+const config = require(path.join("..", "config", "config.json"));
 
-describe("Robot state", () => {
+const maxX = config.table.width;
+const maxY = config.table.height;
+
+describe.only("Robot state", () => {
 
   it("should initialise the state on the table", () => {
-    const state = new RobotState();
-    expect(state).not.to.be.undefined;
+    const state = new RobotState(1, 1, 0);
+    expect(state.x).to.equal(1);
+    expect(state.y).to.equal(1);
+    expect(state.f).to.equal(0);
+    expect(state.isOnTheTable).to.equal(true);
   });
 
   it("should initialise the state off the table when we exeed the table widh", () => {
-    expect(false).to.equal(true);
+    const state = new RobotState(maxX + 1, 0, 0);
+    expect(state.x).to.equal(0);
+    expect(state.y).to.equal(0);
+    expect(state.f).to.equal(0);
+    expect(state.isOnTheTable).to.equal(false);
   });
 
   it("should initialise the state off the table when below the table widh", () => {
-    expect(false).to.equal(true);
+    const state = new RobotState(0, -1, 0);
+    expect(state.x).to.equal(0);
+    expect(state.y).to.equal(0);
+    expect(state.f).to.equal(0);
+    expect(state.isOnTheTable).to.equal(false);
   });
 
   it("should initialise the state off the table when we exeed the table height", () => {
-    expect(false).to.equal(true);
+    const state = new RobotState(0, maxY + 1, 0);
+    expect(state.x).to.equal(0);
+    expect(state.y).to.equal(0);
+    expect(state.f).to.equal(0);
+    expect(state.isOnTheTable).to.equal(false);
   });
 
   it("should initialise the state off the table when below the table height", () => {
-    expect(false).to.equal(true);
+    const state = new RobotState(0, -1, 0);
+    expect(state.x).to.equal(0);
+    expect(state.y).to.equal(0);
+    expect(state.f).to.equal(0);
+    expect(state.isOnTheTable).to.equal(false);
   });
 });
